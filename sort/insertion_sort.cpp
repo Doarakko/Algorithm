68111e1ccde913c20bc86672c1b66bf02ddaa3b2
Doarakko <2wua4nlyi@gmail.com>
2019-11-12 23:08:19 +0900

    add insertion sort

---
+++ sort/insertion_sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int> v)
+{
+    for (int i = 1; i < v.size(); i++)
+    {
+        int x = v[i];
+        for (int j = i - 1; j >= 0 && v[j] > x; j--)
+        {
+            swap(v[j + 1], v[j]);
+        }
+    }
+}
+
+void display(vector<int> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (i < v.size() - 1)
+        {
+            cout << v[i] << " ";
+        }
+        else
+        {
+            cout << v[i] << endl;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    insertionSort(v);
+}
